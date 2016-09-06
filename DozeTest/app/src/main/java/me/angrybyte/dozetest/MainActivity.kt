22597d6ec53691cef08cf480f286a4d612c085d0
Milos Marinkovic <milosmns@gmail.com>
2016-09-06 15:31:25 +0200

    Adding actual app files

--- DozeTest/app/src/main/java/me/angrybyte/dozetest/MainActivity.kt
+++ DozeTest/app/src/main/java/me/angrybyte/dozetest/MainActivity.kt
@@ -1,13 +1,33 @@
 package me.angrybyte.dozetest
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import me.angrybyte.dozetest.DozeLogger.OnCycleFinishedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnCycleFinishedListener {
+
+    val handler: Handler = Handler()
+    var logger: DozeLogger = DozeLogger("Kotlin-LoggerThread")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logger.listener = this
+        logger.start()
+    }
+
+    override fun onCycleFinished(caller: DozeLogger, message: String) {
+        handler.post {
+            logView.text = message
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logger.interrupt()
     }
 
 }
